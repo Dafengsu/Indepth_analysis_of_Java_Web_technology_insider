5d2fbe90f7824b2ba0fd0c776491e52ef59efd89
Dafengsu <1187344209@qq.com>
2019-12-18 00:42:03 +0800

    12.18

--- src/main/java/chapter02/NIOClientTest.java
+++ src/main/java/chapter02/NIOClientTest.java
@@ -7,14 +7,20 @@ import java.net.Socket;
 
 public class NIOClientTest {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("127.0.0.1", 8888);
+        Socket socket = new Socket("127.0.0.1", 8899);
         OutputStream out = socket.getOutputStream();
         InputStream in = socket.getInputStream();
         String s = "hello world";
         out.write(s.getBytes());
         out.close();
         byte[] buff = new byte[1024];
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         in.read(buff);
         System.out.println(new String(buff).trim());
+
     }
 }

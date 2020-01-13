1c17bfcc533b25286bf14b6a0fc51012347222ff
dafengsu <1187344209@qq.com>
2020-01-13 12:06:41 +0800

    1.13

--- src/main/java/Demo/IO/chapter15/NIOUnzipper.java
+++ src/main/java/Demo/IO/chapter15/NIOUnzipper.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.WritableByteChannel;
 import java.util.zip.GZIPInputStream;
@@ -18,10 +19,14 @@ import java.util.zip.GZIPInputStream;
 public class NIOUnzipper {
     public static void main(String[] args) throws IOException {
         FileInputStream fin = new FileInputStream(args[0]);
-
+        int read = fin.read();
+        FileChannel channel = fin.getChannel();
+        channel.read(ByteBuffer.allocate(1));
+        InputStream inputStream = Channels.newInputStream(channel);
+        inputStream.read();
         GZIPInputStream gin = new GZIPInputStream(fin);
         ReadableByteChannel in = Channels.newChannel(gin);
-        InputStream inputStream = Channels.newInputStream(in);
+
         WritableByteChannel out = Channels.newChannel(System.out);
         ByteBuffer buffer = ByteBuffer.allocateDirect(65536);
         while (in.read(buffer) != -1) {
